32762c6567589ffed1254ecc5ae493df9db8960f
John Glover <glover.john@gmail.com>
2011-02-09 15:09:05 +0000

    Update header

--- examples/sinewave.py
+++ examples/sinewave.py
@@ -1,19 +1,5 @@
-# Copyright (c) 2010 John Glover, National University of Ireland, Maynooth
+# Copyright (c) 2010 John Glover
 # 
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
 # Based on code by Daniel Shiffman (http://www.shiffman.net)
 
 from openframeworks import *
@@ -58,4 +44,3 @@ class SineWaveApp(ofBaseApp):
 if __name__ == "__main__":
     ofSetupOpenGL(500, 500, OF_WINDOW)
     ofRunApp(SineWaveApp())
-
