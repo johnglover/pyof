07eae8386e0175ad16db5abb410a2f4e16a7d9e6
John Glover <glover.john@gmail.com>
2011-02-09 15:08:10 +0000

    Tidy up code

--- setup.py
+++ setup.py
@@ -29,60 +29,58 @@ if get_platform() == "unsupported":
     print "       Only Mac OS X is supported."
     exit(1)
 
-# Remove 64-bit flags from distutils sysconfig
-# There doesn't seem to be a nicer way to change these
-config_vars = sysconfig.get_config_vars()
-config_vars['PY_CFLAGS'] = config_vars['PY_CFLAGS'].replace("-arch x86_64 ", "") 
-config_vars['CFLAGS'] = config_vars['CFLAGS'].replace("-arch x86_64 ", "") 
-config_vars['BLDSHARED'] = config_vars['BLDSHARED'].replace("-arch x86_64 ", "") 
-config_vars['LDFLAGS'] = config_vars['LDFLAGS'].replace("-arch x86_64 ", "") 
-config_vars['LDSHARED'] = config_vars['LDSHARED'].replace("-arch x86_64 ", "") 
-
 # include directories
-includes = """fmodex/include/ FreeImage/include/ freetype/include/ 
-              freetype/include/freetype2/
-              glee/include/ poco/include/ rtAudio/include/
-              openFrameworks/app/ openFrameworks/communication/
-              openFrameworks/events/ openFrameworks/graphics/
-              openFrameworks/sound/ openFrameworks/utils/
-              openFrameworks/video/ openFrameworks/""".split()
-for i in range(len(includes)):
-    includes[i] = "../libs/" + includes[i]
+includes = """../libs/fmodex/include/ 
+              ../libs/FreeImage/include/ 
+              ../libs/freetype/include/ 
+              ../libs/freetype/include/freetype2/
+              ../libs/glee/include/ 
+              ../libs/poco/include/ 
+              ../libs/rtAudio/include/
+              ../libs/openFrameworks/app/ 
+              ../libs/openFrameworks/communication/
+              ../libs/openFrameworks/events/ 
+              ../libs/openFrameworks/graphics/
+              ../libs/openFrameworks/sound/ 
+              ../libs/openFrameworks/utils/
+              ../libs/openFrameworks/video/ 
+              ../libs/openFrameworks/""".split()
 
 # source files
-source_files = """app/ofAppGlutWindow.cpp app/ofAppRunner.cpp
-                  communication/ofArduino.cpp communication/ofSerial.cpp
-                  graphics/ofBitmapFont.cpp graphics/ofGraphics.cpp
-                  graphics/ofImage.cpp graphics/ofTexture.cpp
-                  graphics/ofTrueTypeFont.cpp
-                  sound/ofSoundPlayer.cpp sound/ofSoundStream.cpp
-                  utils/ofMath.cpp utils/ofUtils.cpp
-                  video/ofQtUtils.cpp video/ofVideoGrabber.cpp
-                  video/ofVideoPlayer.cpp""".split()
-for i in range(len(source_files)):
-    source_files[i] = "../libs/openFrameworks/" + source_files[i]
-source_files.append("openframeworks/openframeworks.i")
+source_files = """openframeworks/openframeworks.i
+                  ../libs/openFrameworks/app/ofAppGlutWindow.cpp 
+                  ../libs/openFrameworks/app/ofAppRunner.cpp
+                  ../libs/openFrameworks/communication/ofArduino.cpp 
+                  ../libs/openFrameworks/communication/ofSerial.cpp
+                  ../libs/openFrameworks/graphics/ofBitmapFont.cpp 
+                  ../libs/openFrameworks/graphics/ofGraphics.cpp
+                  ../libs/openFrameworks/graphics/ofImage.cpp 
+                  ../libs/openFrameworks/graphics/ofTexture.cpp
+                  ../libs/openFrameworks/graphics/ofTrueTypeFont.cpp
+                  ../libs/openFrameworks/sound/ofSoundPlayer.cpp 
+                  ../libs/openFrameworks/sound/ofSoundStream.cpp
+                  ../libs/openFrameworks/utils/ofMath.cpp 
+                  ../libs/openFrameworks/utils/ofUtils.cpp
+                  ../libs/openFrameworks/video/ofQtUtils.cpp 
+                  ../libs/openFrameworks/video/ofVideoGrabber.cpp
+                  ../libs/openFrameworks/video/ofVideoPlayer.cpp""".split()
 
 # addons
-addon_source_files = """ofxThread/src/ofxThread.cpp""".split()
-for i in range(len(addon_source_files)):
-    addon_source_files[i] = "../addons/" + addon_source_files[i]
-source_files.extend(addon_source_files)
+# addon_source_files = """../addons/ofxThread/src/ofxThread.cpp""".split()
+# source_files.extend(addon_source_files)
 
 # set library directories and add frameworks for OS X
 if get_platform() == "darwin":
-    framework_headers = """/System/Library/Frameworks/OpenGL.framework/Headers
-                           /System/Library/Frameworks/GLUT.framework/Headers
-                           /System/Library/Frameworks/AGL.framework/Headers
-                           /System/Library/Frameworks/ApplicationServices.framework/Headers
-                           /System/Library/Frameworks/AudioToolbox.framework/Headers
-                           /System/Library/Frameworks/Carbon.framework/Headers
-                           /System/Library/Frameworks/CoreAudio.framework/Headers
-                           /System/Library/Frameworks/CoreFoundation.framework/Headers
-                           /System/Library/Frameworks/CoreServices.framework/Headers
-                           /System/Library/Frameworks/Quicktime.framework/Headers""".split()
-    for header in framework_headers:
-        includes.append(header)
+    includes.extend("""/System/Library/Frameworks/OpenGL.framework/Headers
+                       /System/Library/Frameworks/GLUT.framework/Headers
+                       /System/Library/Frameworks/AGL.framework/Headers
+                       /System/Library/Frameworks/ApplicationServices.framework/Headers
+                       /System/Library/Frameworks/AudioToolbox.framework/Headers
+                       /System/Library/Frameworks/Carbon.framework/Headers
+                       /System/Library/Frameworks/CoreAudio.framework/Headers
+                       /System/Library/Frameworks/CoreFoundation.framework/Headers
+                       /System/Library/Frameworks/CoreServices.framework/Headers
+                       /System/Library/Frameworks/Quicktime.framework/Headers""".split())
     link_args = """-framework OpenGL
                    -framework GLUT
                    -framework AGL
@@ -103,6 +101,15 @@ if get_platform() == "darwin":
                    ../libs/poco/lib/osx/PocoNet.a 
                    ../libs/poco/lib/osx/PocoUtil.a 
                    ../libs/poco/lib/osx/PocoXML.a""".split()
+    # Remove any 64-bit flags from distutils sysconfig config_vars, the Quicktime
+    # framework is 32-bit only.
+    # There doesn't seem to be a nicer way to change these values.
+    config_vars = sysconfig.get_config_vars()
+    config_vars['PY_CFLAGS'] = config_vars['PY_CFLAGS'].replace("-arch x86_64 ", "") 
+    config_vars['CFLAGS'] = config_vars['CFLAGS'].replace("-arch x86_64 ", "") 
+    config_vars['BLDSHARED'] = config_vars['BLDSHARED'].replace("-arch x86_64 ", "") 
+    config_vars['LDFLAGS'] = config_vars['LDFLAGS'].replace("-arch x86_64 ", "") 
+    config_vars['LDSHARED'] = config_vars['LDSHARED'].replace("-arch x86_64 ", "") 
 
 # Build the extension
 doc_lines = __doc__.split("\n")
@@ -110,7 +117,7 @@ openframeworks = Extension("openframeworks/_openframeworks",
                            sources = source_files,
                            include_dirs = includes,
                            extra_link_args = link_args,
-                           swig_opts = ['-c++', '-threads']) 
+                           swig_opts = ['-c++']) 
 
 # install setup data_files in the same folder as the module
 # from distutils.command.install import INSTALL_SCHEMES 
