ce98e11730a49cd6144435b0c458b3c3e6c24618
John Glover <glover.john@gmail.com>
2011-02-10 16:18:41 +0000

    Fix typos in oF overview

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 """
 pyof: Python bindings for openFrameworks
 
-Openframeworks is a C++ library designed to assist the creative process by 
+OpenFrameworks is a C++ library designed to assist the creative process by 
 providing a simple and intuitive framework for experimentation.
 
 The library is designed to work as a general purpose glue, and wraps together 
-several commonly used libraries under a tidy interface: openGL for graphics, 
-rtAudio for audio input and output, freeType for fonts,freeImage for image 
-input and output, quicktime for video playing and sequence grabbing.
+several commonly used libraries under a tidy interface: OpenGL for graphics, 
+rtAudio for audio input and output, freeType for fonts, freeImage for image 
+input and output, Quicktime for video playing and sequence grabbing.
 
 For more information see http://www.openframeworks.cc.
 """
